33280aa41fc1e7687bcde34828a3dda3a550d3e4
Carlos <carlosiniesta.apps@gmail.com>
2019-11-04 12:07:03 +0100

    commit create tables: lendings, users and books

---
+++ database/migrations/2019_11_04_104936_create_lendings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLendingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lendings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->dateTime('hour');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lendings');
+    }
+}
